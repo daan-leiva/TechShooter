888ab8cb70e6b7647ddff5e333c5d3ba0795bbf7
Daan Leiva <daan.leiva@gmail.com>
2015-10-17 01:36:12 -0700

    Added menu scenes(place holders). Working on level 1 motion

---
+++ Assets/Scripts/PlayerScrips/PlayerMovement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+    // public variables
+    float speed;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
