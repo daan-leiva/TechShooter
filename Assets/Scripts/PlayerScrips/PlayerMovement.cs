9becc69699ad6299c1c4d04a7f383d88e22cdefb
Daan Leiva <daan.leiva@gmail.com>
2015-10-17 04:24:14 -0700

    Working on level1 basic set up. Character animations

--- Assets/Scripts/PlayerScrips/PlayerMovement.cs
+++ Assets/Scripts/PlayerScrips/PlayerMovement.cs
@@ -3,16 +3,49 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
     // public variables
-    float speed;
+    public float speed = 5;
 
+	// private variables
+	Rigidbody playerRigidBody;
+	Vector3 movement;
+	Animator anim;
+
+	// set up variables
+	void Awake() {
+		playerRigidBody = GetComponent<Rigidbody>();
+		anim = GetComponent<Animator>();
+	}
 
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	void FixedUpdate() {
+		float h = Input.GetAxisRaw("Horizontal");
+
+		Move (h);
+	}
+
+	void Move(float h) {
+		// crate direction for movement vector
+		Vector3 movement = new Vector3(h, 0f, 0f);
+
+		// normalize so that motion depends on speed
+		movement = movement.normalized*speed*Time.deltaTime;
+
+		// move rigid body
+		playerRigidBody.MovePosition(transform.position + movement);
+
+		if(h != 0)
+			anim.SetBool("IsWalking", true);
+		else
+			anim.SetBool("IsWalking", false);
+
 	}
 }
